8fcd94c96625ab43f736607171dd36d99097a350
rjtjs <sajetjars@gmail.com>
2023-11-22 09:23:30 -0800

    moved to ECS from manually managing state

---
+++ src/systems/mod.rs
@@ -0,0 +1,13 @@
+mod entity_renders;
+mod map_renders;
+mod player_input;
+
+use crate::prelude::*;
+
+pub fn build_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(player_input::player_input_system())
+        .add_system(map_renders::map_renders_system())
+        .add_system(entity_renders::entity_render_system())
+        .build()
+}
