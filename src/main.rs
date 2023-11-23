8d940a9400dc2a1c558dd981f19feb71134e881b
rjtjs <sajetjars@gmail.com>
2023-11-22 18:16:25 -0800

    added turns and random movement for enemies

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod map;
 mod map_builder;
 mod spawner;
 mod systems;
+mod turn_state;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -20,6 +21,7 @@ mod prelude {
     pub use crate::map_builder::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
+    pub use crate::turn_state::*;
 
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 80;
@@ -30,7 +32,9 @@ mod prelude {
 struct State {
     ecs: World,
     resources: Resources,
-    systems: Schedule,
+    input_systems: Schedule,
+    player_systems: Schedule,
+    enemy_systems: Schedule,
 }
 
 impl State {
@@ -52,11 +56,14 @@ impl State {
 
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(TurnState::AwaitingInput);
 
         Self {
             ecs,
             resources,
-            systems: build_scheduler(),
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            enemy_systems: build_enemy_scheduler(),
         }
     }
 }
@@ -70,7 +77,20 @@ impl GameState for State {
 
         self.resources.insert(ctx.key);
 
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+        let current_turn_state = self.resources.get::<TurnState>().unwrap().clone();
+
+        match current_turn_state {
+            TurnState::AwaitingInput => self
+                .input_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::PlayerTurn => self
+                .player_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::EnemyTurn => self
+                .enemy_systems
+                .execute(&mut self.ecs, &mut self.resources),
+        }
+
         render_draw_buffer(ctx).expect("render error");
     }
 }
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,15 +1,39 @@
 mod collisions;
+mod end_turn;
 mod entity_renders;
 mod map_renders;
 mod player_input;
+mod random_movement;
 
 use crate::prelude::*;
 
-pub fn build_scheduler() -> Schedule {
+pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
-        .add_system(collisions::collisions_system())
+        .flush()
         .add_system(map_renders::map_renders_system())
         .add_system(entity_renders::entity_render_system())
+        .build()
+}
+
+pub fn build_player_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_renders::map_renders_system())
+        .add_system(entity_renders::entity_render_system())
+        .add_system(end_turn::end_turn_system())
+        .build()
+}
+
+pub fn build_enemy_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(random_movement::random_move_system())
+        .flush()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_renders::map_renders_system())
+        .add_system(entity_renders::entity_render_system())
+        .add_system(end_turn::end_turn_system())
         .build()
 }
